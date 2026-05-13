c3804c9b2f5b221f74092cfdbb7bb226d37c88fd
LokiCode1 <rushikeshkengar648@gmail.com>
2026-05-13 10:58:16 +0530

    Delete src directory

--- src/main/java/com/ems/employeemanagmentsystem/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ems.employeemanagmentsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ems.employeemanagmentsystem.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
