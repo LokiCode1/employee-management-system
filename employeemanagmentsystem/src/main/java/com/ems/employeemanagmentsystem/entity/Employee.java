968bfc53d017e723e28a28a706f3b5ab341bf949
LokiCode1 <rushikeshkengar648@gmail.com>
2026-05-13 12:48:28 +0530

    Add files via upload

---
+++ employeemanagmentsystem/src/main/java/com/ems/employeemanagmentsystem/entity/Employee.java
@@ -0,0 +1,189 @@
+package com.ems.employeemanagmentsystem.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@Column(name = "EMPID")
+	@NotNull(message = "EMPID is mandatory")
+	@JsonProperty("empid")
+	private Integer empId;
+
+	private Integer zipcode;
+
+	private Integer phone;
+
+	@Column(name = "CompaneyId")
+	private Integer companyId;
+
+	@NotBlank
+	@Size(max = 255)
+	private String name;
+
+	@NotBlank
+	@Email
+	@Size(max = 255)
+	private String email;
+
+	@NotBlank
+	@Size(max = 255)
+	private String street;
+
+	@NotBlank
+	@Size(max = 100)
+	private String suite;
+
+	@NotBlank
+	@Size(max = 100)
+	private String city;
+
+	@NotBlank
+	@Size(max = 512)
+	@URL
+	private String website;
+
+	@NotBlank
+	@Size(max = 500)
+	private String catchPhrase;
+
+	@NotBlank
+	@Size(max = 255)
+	private String bc;
+
+	public Employee() {
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public Integer getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(Integer zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public Integer getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getSuite() {
+		return suite;
+	}
+
+	public void setSuite(String suite) {
+		this.suite = suite;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getCatchPhrase() {
+		return catchPhrase;
+	}
+
+	public void setCatchPhrase(String catchPhrase) {
+		this.catchPhrase = catchPhrase;
+	}
+
+	public String getBc() {
+		return bc;
+	}
+
+	public void setBc(String bc) {
+		this.bc = bc;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Employee employee = (Employee) o;
+		return Objects.equals(empId, employee.empId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(empId);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{empId=" + empId + ", name='" + name + "', email='" + email + '\'' + '}';
+	}
+}
